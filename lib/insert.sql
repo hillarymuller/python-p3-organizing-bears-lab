a5b2248f19e3bc0c660798f8400da4a1e7e11f2c
Hillary Muller <hillaryaward@gmail.com>
2023-08-02 18:42:54 -0500

    write sql queries to pass tests

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+    ('Mr. Chocolate', 5, 'F', 'brown', 'happy', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+    ('Rowdy', 5, 'F', 'brown', 'happy', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+    ('Tabitha', 5, 'F', 'brown', 'happy', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+    ('Sergeant Brown', 15, 'F', 'blue', 'happy', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+    ('Melissa', 11, 'F', 'brown', 'happy', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+    ('Grinch', 3, 'M', 'brown', 'happy', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+    ("Wendy", 10, 'F', 'brown', 'happy', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+    (NULL, 5, 'F', 'brown', 'happy', True);
